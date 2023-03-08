b9d970812e9a337bd3a0fe7b0867c65d30889b53
Brunoss21 <80181592+Brunoss21@users.noreply.github.com>
2023-03-08 16:06:31 -0300

    2 files modified

--- src/flow/flow.component.ts
+++ src/flow/flow.component.ts
@@ -1,16 +1,19 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-flow',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './flow.component.html',
   styleUrls: ['./flow.component.css'],
 })
 export class FlowComponent implements OnInit {
+  n: number = 10;
+
   getNumbers() {
-    return [10, 17, 22, 78, 88, 99, 24, 75];
+    return [10, 17, 88, 99, 100];
   }
 
   ngOnInit() {}
